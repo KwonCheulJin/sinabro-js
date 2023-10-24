6b586abf1896b2165e143ef115c7ea9168ad76ce
jin <chkftm12@gmail.com>
2023-10-24 18:11:09 +0900

    solve: filter.test.js

--- ep04/src/filter.test.js
+++ ep04/src/filter.test.js
@@ -6,7 +6,7 @@ describe('filter method - simple', () => {
     const numbers = [1, -2, 3, -4, 5];
 
     // TODO: do something here
-    const positiveNumbers = [];
+    const positiveNumbers = numbers.filter(number => number > 0);
     expect(positiveNumbers).toEqual([1, 3, 5]);
   });
 
@@ -18,7 +18,9 @@ describe('filter method - simple', () => {
     ];
 
     // TODO: do something here
-    const salesEmployees = [];
+    const salesEmployees = employees.filter(
+      employee => employee.department === 'Sales'
+    );
     expect(salesEmployees).toEqual([
       { name: 'John', age: 30, department: 'Sales' },
       { name: 'Jim', age: 40, department: 'Sales' },
@@ -33,7 +35,9 @@ describe('filter method - simple', () => {
     ];
 
     // TODO: do something here
-    const salesEmployeesOver35 = [];
+    const salesEmployeesOver35 = employees.filter(
+      employee => employee.age > 35 && employee.department === 'Marketing'
+    );
     expect(salesEmployeesOver35).toEqual([
       { name: 'Jim', age: 40, department: 'Marketing' },
     ]);
@@ -49,7 +53,9 @@ describe('filter method - simple', () => {
     const targetDepartments = ['Sales', 'Development'];
 
     // TODO: do something here
-    const salesOrDevEmployees = [];
+    const salesOrDevEmployees = employees.filter(employee =>
+      targetDepartments.includes(employee.department)
+    );
     expect(salesOrDevEmployees).toEqual([
       { name: 'John', age: 30, department: 'Sales' },
       { name: 'Jane', age: 35, department: 'Development' },
@@ -60,13 +66,19 @@ describe('filter method - simple', () => {
 describe('filter method - real world', () => {
   it('gets posts from this year', () => {
     // TODO: do something here
-    const postsThisYear = [];
+    const postsThisYear = posts.filter(post => {
+      const postYear = new Date(post.meta.created_at).getFullYear();
+      const currentYear = new Date().getFullYear();
+      return currentYear === postYear;
+    });
     expect(postsThisYear.length).toBe(10);
   });
 
   it('gets posts with "culture" tag', () => {
     // TODO: do something here
-    const postsWithCultureTag = [];
+    const postsWithCultureTag = posts.filter(post =>
+      post.meta.tags.includes('culture')
+    );
     expect(postsWithCultureTag.length).toBe(16);
   });
 
@@ -75,7 +87,16 @@ describe('filter method - real world', () => {
     // new Date('2023-02-03T21:10:00.000Z').toLocaleString('fr-FR')
     //
     // TODO: do something here
-    const tweetsPostedAfter10pm = [];
+    const tweetsPostedAfter10pm = posts.filter(post => {
+      const frenchTimestamp = new Intl.DateTimeFormat('fr-FR', {
+        dateStyle: 'short',
+        timeStyle: 'long',
+        timeZone: 'Europe/Paris',
+      }).format(new Date(post.meta.created_at));
+      return frenchTimestamp.includes('UTC+2')
+        ? +frenchTimestamp.split(' ')[1].split(':')[0] >= 22
+        : null;
+    });
     expect(tweetsPostedAfter10pm.length).toBe(5);
   });
 });
